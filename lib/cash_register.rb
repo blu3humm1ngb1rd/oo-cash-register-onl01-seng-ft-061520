6fe2e4bd4bb413f4385d96b59dd6b7578e2675a6
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-28 18:18:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     #binding.pry
-    @items.times {|x| @items << x}  
+      
     price = price*quantity
     @total = @total + price
     
