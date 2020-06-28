78ddaf93bda386d5f3445ceef22cae589f1ecf84
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-28 18:30:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,12 @@
 require "pry"
 class CashRegister
-  attr_accessor :total, :discount, :items  
+  attr_accessor :total, :discount, :items, :last_transaction 
   
   def initialize(discount = 0)
     @total = 0
     @discount = discount 
     @items = []
+    @last_transaction = last_transaction
   end 
 
   def add_item(title, price, quantity = 1)
